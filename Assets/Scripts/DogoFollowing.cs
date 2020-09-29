4a32fecf022aa8cca545014e597deaad0f5df8b6
Just A Human Being <27690072+JustHumanBeing@users.noreply.github.com>
2020-09-29 10:58:55 +0300

    Implemented DogoDollowing and PlayerRotating

---
+++ Assets/Scripts/DogoFollowing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogoFollowing : MonoBehaviour
+{
+    public Transform target;
+    public float stoppingDistance;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = target.position - transform.position;
+    	float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+    	transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
+    }
+
+    void FixedUpdate() {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
