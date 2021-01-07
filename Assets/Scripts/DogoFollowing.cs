a7f0ea3c23e002ec35674144ca0a8e8870e316be
Just a Human Being <prsanja@gmail.com>
2021-01-07 21:22:55 +0300

    Dogo follows cursor

--- Assets/Scripts/DogoFollowing.cs
+++ Assets/Scripts/DogoFollowing.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class DogoFollowing : MonoBehaviour
 {
-    public Transform target;
+    public Vector3 target;
     public float stoppingDistance;
     public float moveSpeed;
+    public float approximation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 difference = target.position - transform.position;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     	float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
     	transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
     }
 
     void FixedUpdate() {
-        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(Vector2.Distance(transform.position, target) > stoppingDistance + approximation)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        } else if(Vector2.Distance(transform.position, target) < stoppingDistance - approximation)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, -moveSpeed * Time.deltaTime);
         }
     }
 }
